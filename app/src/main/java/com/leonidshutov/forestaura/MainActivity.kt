35d1714aaa2d409356fb2bb25aaddb311b9f8dc5
Leonid Shutov <shizopotam@gmail.com>
2023-07-29 13:31:25 -0700

    add first music file. it plays on Play button

--- app/src/main/java/com/leonidshutov/forestaura/MainActivity.kt
+++ app/src/main/java/com/leonidshutov/forestaura/MainActivity.kt
@@ -1,43 +1,106 @@
 package com.leonidshutov.forestaura
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
-import com.leonidshutov.forestaura.ui.theme.ForestAuraTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ForestAuraTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+            MainScreen(this)
+        }
+    }
+}
+@Composable
+fun MainScreen(activity: ComponentActivity) {
+    var isPlaying by remember { mutableStateOf(false) }
+
+    // Remember the MediaPlayer instance using DisposableEffect
+    val mediaPlayer = remember { MediaPlayer() }
+
+    // DisposableEffect to release the MediaPlayer when the composable is removed
+    DisposableEffect(Unit) {
+        onDispose {
+            mediaPlayer.release()
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {
+                isPlaying = !isPlaying
+                if (isPlaying) {
+                    mediaPlayer.apply {
+                        reset()
+                        val context = activity.applicationContext
+                        val rawResourceId = R.raw.rainforest
+                        val rawFileDescriptor = context.resources.openRawResourceFd(rawResourceId)
+                        setDataSource(rawFileDescriptor.fileDescriptor, rawFileDescriptor.startOffset, rawFileDescriptor.length)
+                        prepare()
+                        start()
+                    }
+                } else {
+                    mediaPlayer.pause()
                 }
+            },
+            colors = ButtonDefaults.buttonColors(
+                contentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        ) {
+            Icon(
+                painter = painterResource(id = if (isPlaying) R.drawable.ic_pause else R.drawable.ic_play),
+                contentDescription = null,
+                modifier = Modifier.size(24.dp),
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = stringResource(id = if (isPlaying) R.string.pause else R.string.play),
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+    }
+
+    // Observe the lifecycle to pause the MediaPlayer when the activity is stopped.
+    val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(lifecycleOwner) {
+        val lifecycleObserver = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_STOP) {
+                mediaPlayer.pause()
             }
+        }
+        lifecycleOwner.lifecycle.addObserver(lifecycleObserver)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(lifecycleObserver)
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    ForestAuraTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    MainScreen(ComponentActivity())
 }
