31078268faa356dce18a554e9d1a5cbdbd61b75f
Leonid Shutov <shizopotam@gmail.com>
2023-07-30 12:22:46 -0700

    add second play button and music file. We can run two simultaneously or one by one now

--- app/src/main/java/com/leonidshutov/forestaura/MainActivity.kt
+++ app/src/main/java/com/leonidshutov/forestaura/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.leonidshutov.forestaura
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,6 +10,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,23 +25,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen(this)
+            MainScreen()
         }
     }
 }
+
 @Composable
-fun MainScreen(activity: ComponentActivity) {
-    var isPlaying by remember { mutableStateOf(false) }
+fun MainScreen() {
+    val mediaPlayer1 = remember { MediaPlayer() }
+    val mediaPlayer2 = remember { MediaPlayer() }
 
-    // Remember the MediaPlayer instance using DisposableEffect
-    val mediaPlayer = remember { MediaPlayer() }
-
-    // DisposableEffect to release the MediaPlayer when the composable is removed
     DisposableEffect(Unit) {
         onDispose {
-            mediaPlayer.release()
+            mediaPlayer1.release()
+            mediaPlayer2.release()
         }
     }
+
+    val context = LocalContext.current
+    var isPlaying1 by remember { mutableStateOf(false) }
+    var isPlaying2 by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -48,48 +53,31 @@ fun MainScreen(activity: ComponentActivity) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Button(
-            onClick = {
-                isPlaying = !isPlaying
-                if (isPlaying) {
-                    mediaPlayer.apply {
-                        reset()
-                        val context = activity.applicationContext
-                        val rawResourceId = R.raw.rainforest
-                        val rawFileDescriptor = context.resources.openRawResourceFd(rawResourceId)
-                        setDataSource(rawFileDescriptor.fileDescriptor, rawFileDescriptor.startOffset, rawFileDescriptor.length)
-                        prepare()
-                        start()
-                    }
-                } else {
-                    mediaPlayer.pause()
-                }
-            },
-            colors = ButtonDefaults.buttonColors(
-                contentColor = MaterialTheme.colorScheme.onPrimary
-            )
-        ) {
-            Icon(
-                painter = painterResource(id = if (isPlaying) R.drawable.ic_pause else R.drawable.ic_play),
-                contentDescription = null,
-                modifier = Modifier.size(24.dp),
-                tint = MaterialTheme.colorScheme.onPrimary
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = stringResource(id = if (isPlaying) R.string.pause else R.string.play),
-                fontSize = 18.sp,
-                color = MaterialTheme.colorScheme.onPrimary
-            )
-        }
+        MediaButton(
+            isPlaying = isPlaying1,
+            mediaPlayer = mediaPlayer1,
+            context = context,
+            rawResourceId = R.raw.rainforest,
+            onToggle = { isPlaying1 = it }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        MediaButton(
+            isPlaying = isPlaying2,
+            mediaPlayer = mediaPlayer2,
+            context = context,
+            rawResourceId = R.raw.humpbackwhale,
+            onToggle = { isPlaying2 = it }
+        )
     }
 
-    // Observe the lifecycle to pause the MediaPlayer when the activity is stopped.
     val lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current
     DisposableEffect(lifecycleOwner) {
         val lifecycleObserver = LifecycleEventObserver { _, event ->
             if (event == Lifecycle.Event.ON_STOP) {
-                mediaPlayer.pause()
+                mediaPlayer1.pause()
+                mediaPlayer2.pause()
             }
         }
         lifecycleOwner.lifecycle.addObserver(lifecycleObserver)
@@ -99,8 +87,48 @@ fun MainScreen(activity: ComponentActivity) {
     }
 }
 
+@Composable
+fun MediaButton(
+    isPlaying: Boolean,
+    mediaPlayer: MediaPlayer,
+    context: Context,
+    rawResourceId: Int,
+    onToggle: (Boolean) -> Unit
+) {
+    Button(
+        onClick = {
+            onToggle(!isPlaying) // Toggle the state before handling playback
+            if (!isPlaying) { // Check the updated state
+                mediaPlayer.reset()
+                val rawFileDescriptor = context.resources.openRawResourceFd(rawResourceId)
+                mediaPlayer.setDataSource(rawFileDescriptor.fileDescriptor, rawFileDescriptor.startOffset, rawFileDescriptor.length)
+                mediaPlayer.prepare()
+                mediaPlayer.start()
+            } else {
+                mediaPlayer.pause()
+            }
+        },
+        colors = ButtonDefaults.buttonColors(
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    ) {
+        Icon(
+            painter = painterResource(id = if (isPlaying) R.drawable.ic_pause else R.drawable.ic_play),
+            contentDescription = null,
+            modifier = Modifier.size(24.dp),
+            tint = MaterialTheme.colorScheme.onPrimary
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = stringResource(id = if (isPlaying) R.string.pause else R.string.play),
+            fontSize = 18.sp,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MainScreen(ComponentActivity())
+    MainScreen()
 }
